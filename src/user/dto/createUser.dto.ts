cea2600ab62a6b13578e4aba7ea56be8c9d5315a
Apankanti <apankanti@paktolus.com>
2023-12-08 15:36:37 +0530

    Frontend-Backend Connection Done data saved to db

--- src/user/dto/createUser.dto.ts
+++ src/user/dto/createUser.dto.ts
@@ -3,11 +3,12 @@
 
 // import { IsArray, IsEmail, IsNotEmpty, IsNumber } from 'class-validator';
 
-export class CreateUserDto { 
+export class CreateUserDto {
     name: string;
     number: string;
     email: string;
-    company: string;
-    services:string[];
-    budget: string;
+    companyName: string;
+    selectedService: string;
+    projectBudget: string;
+    requested_at: Date;
 }
